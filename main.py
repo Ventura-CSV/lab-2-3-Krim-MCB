4080d98f9e23a965b1944099238c9b286c0d698a
Krim-MCB <Kareem_Ismail1@my.vcccd.edu>
2024-06-28 00:18:20 -0700

    Lab 2-2

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 def main():
-    var1=100
-    var2=90
-    var3=110
-    total=var1+var2+var3
+    val1=int(input("Enter Val1:"))
+    val2=int(input("Enter Val2:"))
+    val3=int(input("Enter Val1:"))
+    total=val1+val2+val3
     average = total/3 
     print(f'Total: {total}')
     print(f'Average:{average:.2f}')
+    total_result=total 
+    average_result=average
     ##################################################
     # Comlete your code here
     ##################################################
@@ -18,7 +20,7 @@ def main():
     ########################################
     # Do not delete the return statement
     ########################################
-    return total, average
+    return total_result, average_result
 
 if __name__ == '__main__':
     main()
