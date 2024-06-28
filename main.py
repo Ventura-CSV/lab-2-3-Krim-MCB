0f996f796231a4cdcc818151b92a4caa19fe0177
Krim-MCB <Kareem_Ismail1@my.vcccd.edu>
2024-06-27 23:14:35 -0700

    Lab 2-2 sync #2

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
 def main():
-    val1=100    
-    val2=90
-    val3=110
-    total= val1+val2+val3
-    average = total/3
-    print(f'Total:{total}')
+    var1=100
+    var2=90
+    var3=110
+    total=var1+var2+var3
+    average = total/3 
+    print(f'Total: {total}')
     print(f'Average:{average:.2f}')
-    total_result = total
-    average_result = average
-    return total, average
     ##################################################
     # Comlete your code here
     ##################################################
-"""
+    """
     Use following variables to save your result
     total 
     average
@@ -20,9 +17,8 @@ def main():
 
     ########################################
     # Do not delete the return statement
-    ######################################## 
-
-
+    ########################################
+    return total, average
 
 if __name__ == '__main__':
-        main()
+    main()
